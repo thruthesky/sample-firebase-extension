935aeb468c2d8c72436ddea3029c54f3e93b317d
JaeHo Song <thruthesky@gmail.com>
2023-06-29 12:11:28 +0800

    sample firebase extension

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,28 +1,27 @@
-/*
- * This template contains a HTTP function that responds
- * with a greeting when called
- *
- * Reference PARAMETERS in your functions code with:
- * `process.env.<parameter-name>`
- * Learn more about building extensions in the docs:
- * https://firebase.google.com/docs/extensions/publishers
- */
+import { initializeApp } from "firebase-admin";
+import { EventContext, database, logger } from "firebase-functions/v1";
 
-import * as functions from "firebase-functions";
+initializeApp();
 
-exports.greetTheWorld = functions.https.onRequest(
-  (req: functions.Request, res: functions.Response) => {
-    // Here we reference a user-provided parameter
-    // (its value is provided by the user during installation)
-    // 사용자가 입력한 파라메타 값 GREETING 이, 설치를 할 때 전달이되어져 온다. 실행될때 마다 값이 전달되어져 온다?
-    const consumerProvidedGreeting = process.env.GREETING;
+// Listens for new messages added to /messages/{pushId}/original and creates an
+// uppercase version of the message to /messages/{pushId}/uppercase
+// for all databases in 'us-central1'
+export const makeuppercase = database
+  .ref("/messages/{pushId}/uppercase")
+  .onCreate(async (snapshot: database.DataSnapshot, context: EventContext<{
+    pushId: string;
+  }>) => {
+    // Grab the current value of what was written to the Realtime Database.
+    const original = snapshot.val();
 
-    // And here we reference an auto-populated parameter
-    // (its value is provided by Firebase after installation)
-    // 파이어베이스 자동으로 제공하는 파라메타 값 EXT_INSTANCE_ID. 이 값은 설치 후에, 제공된다.
-    const instanceId = process.env.EXT_INSTANCE_ID;
 
-    const greeting = `${consumerProvidedGreeting} everyone 234 from ${instanceId}`;
 
-    res.send(greeting);
+    // Convert it to upper case.
+    logger.log("Uppercasing", context.params.pushId, original);
+    const uppercase = original.toUpperCase();
+
+    // Setting an "uppercase" sibling in the Realtime Database.
+    const upperRef = snapshot.ref.parent!.child("upper");
+    await upperRef.set(uppercase);
   });
+
